ecf6f25d94288886044d18c31a5c468a9ee94b01
Kevin McGee(Mac Book Pro) <run2dos@gmail.com>
2015-04-12 11:08:16 -0700

    Another Test

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ print('Hello Git')
 print('Hello Git from my desktop!')
 print('Hello back form my MBP I really need to make '
       'an automation system for creating projects.')
+
+print('This is another test!')
